692a655be564ed888c3bbff3ed5d030d8d3b1d47
RLARN <msjdoc0645@naver.com>
2023-03-22 20:02:42 +0900

    PostsApiControllerTest 테스트 성공 오류 수정. PostsService.java

--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -81,7 +81,7 @@ public class PostsApiControllerTest {
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(title);
-        assertThat(all.get(0).getContent()).isEqualTo(content);
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
